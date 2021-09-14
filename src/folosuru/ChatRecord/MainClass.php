acd39c12c8359354792820e492eaf0b90112d112
folosuru <77926718+folosuru@users.noreply.github.com>
2021-09-14 20:04:47 +0900

    Add files via upload

--- src/folosuru/ChatRecord/MainClass.php
+++ src/folosuru/ChatRecord/MainClass.php
@@ -6,6 +6,9 @@ namespace folosuru\ChatRecord;
 
 use pocketmine\command\Command;
 use pocketmine\command\CommandSender;
+use pocketmine\event\player\PlayerDeathEvent;
+use pocketmine\event\player\PlayerJoinEvent;
+use pocketmine\event\player\PlayerQuitEvent;
 use pocketmine\plugin\PluginBase;
 use pocketmine\utils\TextFormat;
 use pocketmine\event\player\PlayerChatEvent;
@@ -15,6 +18,8 @@ use pocketmine\player;
 
 
 class MainClass extends PluginBase implements Listener{
+
+	private $chatlog;
 
 	public function onLoad() : void{
 	}
@@ -38,17 +43,17 @@ class MainClass extends PluginBase implements Listener{
 	public function onPlayerCommand(PlayerCommandPreprocessEvent $event) {
 		if ($event->isCancelled()) return;
 		$args = explode(" ",$event->getMessage());
-		$command = array_shift($args)	//	[/tell] folosuru hogehoge
-		if (strtolower($command) === "/tell" or strtolower($command === "/w" or strtolower($command) === "/msg"){
+		$command = array_shift($args);	//	[/tell] folosuru hogehoge
+		if (strtolower($command) === "/tell" or strtolower($command) === "/w" or strtolower($command) === "/msg"){
 			if (count($args) < 2 ) return;
 			$player = $this->getServer()->getPlayer(array_shift($args));	//	/tell [folosuru] hogehoge
 			if($event->getPlayer() === $player) {
 				return;
 			}
 			if($player instanceof Player) $this->getLogger()->info("text");
-			$this->chatlog = $this->chatlog."[".date("Y-m/d H:i:s")."]  <".$event->getPlayer()->getName()." -> ".$player->getName()."> ".implode(" ", $args)."\n";
+			$this->chatlog = $this->chatlog."[".date("Y-m/d H:i:s")."]  <".$event->getPlayer()->getName()." => ".$player->getName()."> ".implode(" ", $args)."\n";
 		}
-		if (strtolower($command) === "/me"){
+		if (strtolower($command) === "/me" or strtolower($command) === "/say"){
 			$this->chatlog = $this->chatlog."[".date("Y-m/d H:i:s")."]  <".$event->getPlayer()->getName()."> ".implode(" ", $args)."\n";
 		}
 
@@ -57,4 +62,14 @@ class MainClass extends PluginBase implements Listener{
 	public function onChat(PlayerChatEvent $event){
 		$this->chatlog = $this->chatlog."[".date("Y-m/d H:i:s")."]  <".$event->getPlayer()->getName()."> ".$event->getMessage()."\n";
 	}
+	public function onJoin(PlayerJoinEvent $event){
+		$this->chatlog = $this->chatlog."[".date("Y-m/d H:i:s")."]  [Player_Join]".$event->getPlayer()->getName()."\n";
+	}
+	public function onQuit(PlayerQuitEvent $event){
+		$this->chatlog = $this->chatlog."[".date("Y-m/d H:i:s")."]  [Player_Quit]".$event->getPlayer()->getName()."\n";
+	}
+	public function onDeath(PlayerDeathEvent $event){
+		$this->chatlog = $this->chatlog."[".date("Y-m/d H:i:s")."]  [Player_Death]".$event->getPlayer()->getName()."\n";
+	}
+
 }
