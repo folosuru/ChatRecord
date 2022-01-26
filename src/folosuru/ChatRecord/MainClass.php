c46910e154b371793aadd0d75c398fae38c42e3f
folosuru <77926718+folosuru@users.noreply.github.com>
2022-01-26 16:03:48 +0900

    めっちゃ適当に作ってたせいでエラーがすごいことになってた

--- src/folosuru/ChatRecord/MainClass.php
+++ src/folosuru/ChatRecord/MainClass.php
@@ -4,13 +4,7 @@ declare(strict_types=1);
 
 namespace folosuru\ChatRecord;
 
-use pocketmine\command\Command;
-use pocketmine\command\CommandSender;
-use pocketmine\event\player\PlayerDeathEvent;
-use pocketmine\event\player\PlayerJoinEvent;
-use pocketmine\event\player\PlayerQuitEvent;
 use pocketmine\plugin\PluginBase;
-use pocketmine\utils\TextFormat;
 use pocketmine\event\player\PlayerChatEvent;
 use pocketmine\event\player\PlayerCommandPreprocessEvent;
 use pocketmine\event\Listener;
@@ -19,7 +13,7 @@ use pocketmine\player;
 
 class MainClass extends PluginBase implements Listener{
 
-
+	private $chatlog;
 
 	public function onLoad() : void{
 	}
@@ -32,44 +26,40 @@ class MainClass extends PluginBase implements Listener{
 
 	}
 	public function onDisable() : void{
-		$path = "plugin_data/ChatRecord/" . date("Y/m/d/");
+		$path = $this->getDataFolder() . date("Y/m/d/");
 		if (!file_exists($path)) {
 			mkdir($path, 0777, true);
 		}
-		$file = $path."chatlog-" . date("H").".txt";
+		$file = $path."chatlog-" . date("H");
 		file_put_contents($file,$this->chatlog );
 	}
 
 	public function onPlayerCommand(PlayerCommandPreprocessEvent $event) {
 		if ($event->isCancelled()) return;
-		$args = explode(" ",$event->getMessage());
+		$message = $event->getMessage();
+		$this->getLogger()->info($message);
+		$args = explode(" ",$message);
 		$command = array_shift($args);	//	[/tell] folosuru hogehoge
 		if (strtolower($command) === "/tell" or strtolower($command) === "/w" or strtolower($command) === "/msg"){
-			if (count($args) < 2 ) return;
+			if (count($args) < 3 ) return;
+			$this->getLogger()->info("/tell");
 			$player = $this->getServer()->getPlayer(array_shift($args));	//	/tell [folosuru] hogehoge
 			if($event->getPlayer() === $player) {
 				return;
 			}
 			if($player instanceof Player) $this->getLogger()->info("text");
-			$this->chatlog = $this->chatlog."[".date("Y-m/d H:i:s")."]  <".$event->getPlayer()->getName()." => ".$player->getName()."> ".implode(" ", $args)."\n";
+			$this->chatlog = $this->chatlog."[".date("Y-m/d H:i:s")."]  <".$event->getPlayer()->getName()." -> ".$player->getName()."> ".implode(" ", $args)."\n";
 		}
-		if (strtolower($command) === "/me" or strtolower($command) === "/say"){
-			$this->chatlog = $this->chatlog."[".date("Y-m/d H:i:s")."]  <".$event->getPlayer()->getName()."> ".implode(" ", $args)."\n";
+		if (strtolower($command) === "/me") {
+			$this->chatlog = $this->chatlog . "[" . date("Y-m/d H:i:s") . "]  <" . $event->getPlayer()->getName() . "> " . implode(" ", $args) . "\n";
 		}
+
+		//$this->chatlog = $this->chatlog."[".date("Y-m/d H:i:s")."]  <".$event->getPlayer()->getName()." -> ". "> ";
+
 
 	}
 
 	public function onChat(PlayerChatEvent $event){
 		$this->chatlog = $this->chatlog."[".date("Y-m/d H:i:s")."]  <".$event->getPlayer()->getName()."> ".$event->getMessage()."\n";
 	}
-	public function onJoin(PlayerJoinEvent $event){
-		$this->chatlog = $this->chatlog."[".date("Y-m/d H:i:s")."]  [Player_Join]".$event->getPlayer()->getName()."\n";
-	}
-	public function onQuit(PlayerQuitEvent $event){
-		$this->chatlog = $this->chatlog."[".date("Y-m/d H:i:s")."]  [Player_Quit]".$event->getPlayer()->getName()."\n";
-	}
-	public function onDeath(PlayerDeathEvent $event){
-		$this->chatlog = $this->chatlog."[".date("Y-m/d H:i:s")."]  [Player_Death]".$event->getPlayer()->getName()."\n";
-	}
-
 }
